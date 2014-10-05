84afb62ea0c801bfcdf48f8ffbc3c223e3bc2bbb
AnnGuden <anna@MacBook-Air-Anna.local>
2014-10-05 15:15:57 +0300

    edit task4

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,56 +8,52 @@
  * Controller of the testApp
  */
 angular.module('testApp')
-  .controller('MainCtrl', function ($scope, $filter, $http, $resource, Posts) {
-      $scope.showArticles = function () {
-        $scope.articles = Posts.query();
-      };
-      
-      $scope.showArticles();
-      
+  .controller('MainCtrl', function ($scope, $filter, Posts) {
+
+      $scope.articles = Posts.query();
+
       $scope.addArticle = function () {
       if ($scope.newArticle._id) {
         Posts.update({ id: $scope.newArticle._id }, $scope.newArticle, function() {
-          $scope.showArticles();
           $scope.resetArticleForm();
           $scope.toggleArticleForm();
         });
       }else{
         $scope.newArticle.date = $filter('date')(new Date(), 'yyyy/MM/dd h:mm a');
-        Posts.save($scope.newArticle, function() {
-          $scope.showArticles();
+        Posts.save($scope.newArticle, function(response) {
+          $scope.articles.push(response);
           $scope.resetArticleForm();
           $scope.toggleArticleForm();
         });
       }
     };
-      
+
       $scope.editArticle = function (article) {
           $scope.newArticle = article;
           $scope.editingArticle = true;
           $scope.toggleArticleForm();
       };
-      
+
       $scope.deleteArticle = function (article) {
           Posts.delete({ id: article._id }, function() {
-            $scope.showArticles();
+            $scope.articles = Posts.query();
           });
       };
-            
+
       $scope.resetArticleForm = function () {
           $scope.articleForm.$setPristine();
           $scope.newArticle = {};
           $scope.editingArticle = false;
      };
-      
+
      $scope.toggleArticleForm = function () {
       $scope.articleFormVisible = !$scope.articleFormVisible;
      };
-      
+
       $scope.closeArticleForm = function () {
           $scope.resetArticleForm();
           $scope.toggleArticleForm();
-      }
-      
+      };
+
 
   });
